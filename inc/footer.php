c88d6ad58d9ab710079cc6486c9ba08c55f540ee
lazarkolarovking <lazar.kolarov33@hotmail.com>
2019-03-21 20:35:52 +1000

    first commit for TeamApp

---
+++ inc/footer.php
@@ -0,0 +1,5 @@
+<br /><br /><br /><br /><br />
+<div class="footer">BestService - <strong>Build From Scratch Series</strong>. &copy; <?php echo date('Y'); ?>.</div>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,3 @@
+## CRM
+
+Is for Teamapp by BestService
